f472404055be6b4847df85fad0eea6b77ae34580
QAQ <274381441@qq.com>
2020-01-11 23:10:12 +0800

    firstgit

---
+++ gittest/src/com/itheima/gittest/Fristgit.java
@@ -0,0 +1,14 @@
+package com.itheima.gittest;
+
+public class Fristgit {
+    public static void main(String[] args) {
+        System.out.println("hello git.....");
+        System.out.println("hello git.....");
+        System.out.println("hello git.....");
+        System.out.println("hello git.....");
+        System.out.println("hello git.....");
+        System.out.println("hello git.....");
+        System.out.println("hello git.....");
+        System.out.println("hello git.....");
+    }
+}
